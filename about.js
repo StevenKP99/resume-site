841aeb6ac157156da6f9c0b498e03de5308fea16
Steven Puckett <steven..k.puckett24@gmail.com>
2026-01-30 02:13:24 -0600

    Add some SEO support. Changed Header back to static html to assists with this.

--- about.js
+++ about.js
@@ -1,5 +1,3 @@
-import { renderHeader } from "./src/components/Header/header.js";
 import { renderAboutSection } from "./src/components/AboutSection/aboutSection.js";
 
-renderHeader('.profile-header');
 renderAboutSection('.about-section');
